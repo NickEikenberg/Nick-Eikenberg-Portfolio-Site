8cd4a817176dac1112d0cad8cad317caf16eb58d
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-04 02:24:58 -0500

    First project added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Header, Banner, Footer, Project } from './components/Index';
 
 const App = () => {
   return (
-    <div className="p-5 bg-gradient-to-b from-red-200 to-purple-800 text-white">
-      <div className="border-2 border-gray-800 bg-gray-900 rounded-md p-5">
+    <div className="p-5 bg-gradient-to-b from-red-200 to-purple-800 text-black">
+      <div className="border-2 border-gray-800 bg-gray-300 rounded-md p-5">
         <Header />
         <h1>Hello Portfolio</h1>
         <Banner />
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { FaReact, FaNode } from 'react-icons/fa';
+import {
+  SiJavascript,
+  SiTailwindcss,
+  SiHtml5,
+  SiCss3,
+  SiMongodb,
+} from 'react-icons/si';
 
 const Project = () => {
   return (
     <div className="flex m-16">
       <div className="w-1/2">
-        <h1 className="text-5xl">Project Name</h1>
-        <h2 className="text-2xl">Full-Stack Application | React</h2>
+        <h1 className="text-6xl">Project Name</h1>
+        <h2 className="text-3xl">Full-Stack Application | React</h2>
         <div className="h-3/4">
-          <p className=" bg-black text-white">
+          <p className="text-xl p-6 bg-black text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -18,18 +26,40 @@ const Project = () => {
           </p>
         </div>
         <div>
-          <h1>Technologies Used</h1>
-          <div className="flex overflow-hidden">
-            <div>TECHNOLOGYTECHNOLOGY</div>
-            <div>TECHNOLOGYTECHNOLOGY</div>
-            <div>TECHNOLOGYTECHNOLOGY</div>
+          <h1 className="w-full bg-black text-white text-center text-3xl">
+            Technologies Used
+          </h1>
+          <div className="flex overflow-hidden text-6xl justify-center py-2 space-x-4 bg-black">
+            <div>
+              <SiTailwindcss className="text-icon-tailwind" />
+            </div>
+            <div>
+              <SiJavascript className="text-icon-js" />
+            </div>
+            <div>
+              <FaReact className="text-icon-react" />
+            </div>
+            <div>
+              <SiHtml5 className="text-icon-html" />
+            </div>
+            <div>
+              <SiCss3 className="text-icon-css" />
+            </div>
+            <div>
+              <FaNode className="text-icon-node" />
+            </div>
+            <div>
+              <SiMongodb className="text-icon-mongodb" />
+            </div>
           </div>
         </div>
       </div>
       <div className="mockup-phone">
         <div className="camera"></div>
         <div className="display">
-          <div className="artboard phone-1 artboard-demo">Hi.</div>
+          <div className="artboard phone-1 artboard-demo">
+            <img src="https://i.imgur.com/voqeIDC.png" alt="Unifriends"></img>
+          </div>
         </div>
       </div>
     </div>

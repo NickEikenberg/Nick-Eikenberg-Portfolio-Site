32a4667f019f6b6b9e69e79f52c0c27f7f824e98
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-04 01:33:22 -0500

    More styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 const Project = () => {
   return (
-    <div className="flex">
+    <div className="flex m-16">
       <div className="w-1/2">
-        <h1 className="">Project Name</h1>
-        <h2>Full-Stack Application | React</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <h1 className="text-5xl">Project Name</h1>
+        <h2 className="text-2xl">Full-Stack Application | React</h2>
+        <div className="h-3/4">
+          <p className=" bg-black text-white">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
         <div>
           <h1>Technologies Used</h1>
           <div className="flex overflow-hidden">

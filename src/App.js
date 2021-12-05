4f4a551bc9e8f1283fa3dbd2991c4f17c2e5dbbd
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-04 20:31:08 -0500

    Stlying changes and removing animation library

--- src/App.js
+++ src/App.js
@@ -7,24 +7,13 @@ import {
   AboutMe,
   Contact,
 } from './components/Index';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const App = () => {
-  const [showBanner, setShowBanner] = useState(true);
-
-  const hideWelcomeBanner = () => {
-    setTimeout(() => {
-      setShowBanner(false);
-    }, 5000);
-  };
-
-  hideWelcomeBanner();
-
   return (
     <div className="p-5 bg-gradient-to-b from-yellow-100 via-purple-400 to-red-100 text-black font-josefin">
       <div className="p-5">
-        {!showBanner && <Header />}
-        {showBanner && <Banner />}
+        <Header />
+        <Banner />
         <AboutMe />
         <Project />
         <Contact />
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Contact = () => {
+  const [showText, setShowText] = useState(false);
+
+  const showContactText = () => {
+    setTimeout(() => {
+      setShowText(true);
+    }, 2000);
+  };
+
   return (
     <div name="contact">
       <div className="h-screen text-center flex justify-center items-center flex-col">
         <h1 className="text-7xl">CONTACT ME</h1>
       </div>
+      {showText && <h1 class="text-4xl">SHOW ME</h1>}
+      <button onClick={showContactText}>CLICK</button>
     </div>
   );
 };
